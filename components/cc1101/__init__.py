5194e212c30216702330618076c6d5a3fc33461a
Tom Scholten <git@scholten.nu>
2024-12-26 18:54:25 +0100

    Imports

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -1,7 +1,7 @@
 from esphome import pins
 from esphome.components import button, fan
-from fan import fan
-from button import button
+from .fan import fan
+from .button import button
 import esphome.config_validation as cv
 
 from esphome.cpp_helpers import gpio_pin_expression
