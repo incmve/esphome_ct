8a45cad080a0ba296330d42b9f04fb43d45ba9c1
Tom Scholten <git@scholten.nu>
2024-12-26 19:08:09 +0100

    Rework setup

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -10,6 +10,10 @@ cc1101fan_ns = cg.esphome_ns.namespace("cc1101fan")
 CC1101Fan = cc1101fan_ns.class_("CC1101Fan", cg.PollingComponent)
 
 MAP_OFF_TO_ZERO = "map_off_to_zero"
+CONF_FAN = "fan"
+CONF_BUTTON = "button"
+CONF_BUTTONS = "buttons"
+CONF_COMMAND = "command"
 
 # Define mapping manually
 FAN_RESTORE_MODE_OPTIONS = {
@@ -18,7 +22,7 @@ FAN_RESTORE_MODE_OPTIONS = {
 }
 
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend({
-  cv.Required("fan"): fan.FAN_SCHEMA.extend({
+  cv.Required(CONF_FAN): fan.FAN_SCHEMA.extend({
     cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(CC1101Fan),
     cv.Required(CONF_DATA_PIN): cv.All(pins.internal_gpio_input_pin_schema),
     cv.Required(MAP_OFF_TO_ZERO): cv.boolean,
@@ -27,33 +31,30 @@ CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend({
     cv.Optional(CONF_DISABLED_BY_DEFAULT, default=False): cv.boolean,
     cv.Optional(CONF_RESTORE_MODE, default="ALWAYS_ON"): cv.enum(FAN_RESTORE_MODE_OPTIONS),
  }),
- cv.Optional("buttons"): cv.ensure_list({
+ cv.Optional(CONF_BUTTONS): cv.ensure_list({
     cv.Required(CONF_NAME): cv.string,
-    cv.Required("command"): cv.int_,
+    cv.Required(CONF_COMMAND): cv.int_,
 #    cv.Required(CONF_DATA_PIN): cv.All(pins.internal_gpio_input_pin_schema),
  }),
 })
 
 async def to_code(config):
-    var = cg.new_Pvariable(config["fan"][CONF_OUTPUT_ID])
+    if CONF_FAN not in config:
+        raise ValueError("Fan configuration is required.")
+
+    var = cg.new_Pvariable(config[CONF_FAN][CONF_OUTPUT_ID], config[CONF_FAN][CONF_SPEED_COUNT], config[CONF_FAN][MAP_OFF_TO_ZERO])
     await cg.register_component(var, config)
+    await fan.register(fan(var, config[CONF_FAN])
 
-    if "fan" not in config:
-        raise Exception
 
-    data_pin = await gpio_pin_expression(config["fan"][CONF_DATA_PIN])
+    data_pin = await gpio_pin_expression(config[CONF_FAN][CONF_DATA_PIN])
     cg.add(var.set_data_pin(data_pin))
+    cg.add(var.set_rf_handler(rf_handler))
 
-    fan_component = cg.new_Pvariable(config["fan"][CONF_SPEED_COUNT], config["fan"][MAP_OFF_TO_ZERO])
-    cg.add(fan_component.set_rf_handler(rf_handler))
-
-    for conf in config.get("buttons", []):
-      data_pin = await gpio_pin_expression(config["fan"][CONF_DATA_PIN])
-      button_component = cg.new_Pvariable(conf[CONF_NAME], rf_handler, data_pin, conf["command"])
+    for conf in config.get(CONF_BUTTONS, []):
+      button_component = cg.new_Pvariable(conf[CONF_NAME], rf_handler, conf[CONF_COMMAND])
+      await cg.register_component(button_component, conf)
       cg.add(button_component)
-
-    if CONF_PRESET_MODES in config:
-        cg.add(var.set_preset_modes(config[CONF_PRESET_MODES]))
 
     return var
 
