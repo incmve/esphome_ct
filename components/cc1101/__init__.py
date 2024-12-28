0a9f32747988288c2ec9d64972ae3ceb480c04c2
Tom Scholten <git@scholten.nu>
2024-12-28 21:24:55 +0100

    Re-approach

--- components/cc1101/__init__.py
+++ components/cc1101/__init__.py
@@ -4,17 +4,23 @@ import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.cpp_helpers import gpio_pin_expression
 
-from esphome.const import CONF_NAME, CONF_OUTPUT_ID, CONF_DATA_PIN, CONF_SPEED_COUNT, CONF_DISABLED_BY_DEFAULT, CONF_RESTORE_MODE
+from esphome.const import CONF_ID, CONF_NAME, CONF_OUTPUT_ID, CONF_DATA_PIN, CONF_SPEED_COUNT, CONF_DISABLED_BY_DEFAULT, CONF_RESTORE_MODE
 
-cc1101fan_ns = cg.esphome_ns.namespace("cc1101fan")
-CC1101Fan = cc1101fan_ns.class_("CC1101Fan", cg.PollingComponent)
+MULTI_CONF = True
 
-MAP_OFF_TO_ZERO = "map_off_to_zero"
+AUTO_LOAD = [ 'button', 'fan' ]
+
+cc1101_ns = cg.esphome_ns.namespace('cc1101')
+CC1101Fan = cc1101_ns.class_('CC1101Fan', cg.PollingComponent, fan.Fan)
+CC1101Button = cc1101_ns.class_('CC1101Button', button.Button)
+RFHandler = cc1101_ns.class_('RFHandler', cg.Component)
+
 CONF_FAN = "fan"
 CONF_BUTTON = "button"
 CONF_BUTTONS = "buttons"
 CONF_COMMAND = "command"
 CONF_RF_HANDLER_ID = "rf_handler_id"
+MAP_OFF_TO_ZERO = "map_off_to_zero"
 
 # Define mapping manually
 FAN_RESTORE_MODE_OPTIONS = {
@@ -25,7 +31,7 @@ FAN_RESTORE_MODE_OPTIONS = {
 CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend({
   cv.Required(CONF_FAN): fan.FAN_SCHEMA.extend({
     cv.GenerateID(CONF_OUTPUT_ID): cv.declare_id(CC1101Fan),
-    cv.GenerateID(CONF_RF_HANDLER_ID): cv.use_id("rf_handler"),
+    cv.GenerateID(CONF_RF_HANDLER_ID): cv.declare_id(RFHandler),
     cv.Required(CONF_DATA_PIN): cv.All(pins.internal_gpio_input_pin_schema),
     cv.Required(MAP_OFF_TO_ZERO): cv.boolean,
     cv.Optional(CONF_NAME, default="Domestic Fan"): str,
@@ -34,6 +40,7 @@ CONFIG_SCHEMA = cv.COMPONENT_SCHEMA.extend({
     cv.Optional(CONF_RESTORE_MODE, default="ALWAYS_ON"): cv.enum(FAN_RESTORE_MODE_OPTIONS),
  }),
  cv.Optional(CONF_BUTTONS): cv.ensure_list({
+    cv.GenerateID(): cv.declare_id(CC1101Button),
     cv.Required(CONF_NAME): cv.string,
     cv.Required(CONF_COMMAND): cv.int_,
 #    cv.Required(CONF_DATA_PIN): cv.All(pins.internal_gpio_input_pin_schema),
@@ -55,9 +62,10 @@ async def to_code(config):
     cg.add(var.set_data_pin(data_pin))
 
     for conf in config.get(CONF_BUTTONS, []):
-      button_component = cg.new_Pvariable(conf[CONF_NAME], rf_handler, conf[CONF_COMMAND])
+      button_component = cg.new_Pvariable(conf[CONF_ID], rf_handler, conf[CONF_COMMAND])
       await cg.register_component(button_component, conf)
       cg.add(button_component)
+      await cg.register_component(button_component, conf)
 
     return var
 
