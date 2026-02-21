5ece81a3f5f3cf37905b6b0c36aa2827dbb4cbb4
incmve <incmve@users.noreply.github.com>
2026-02-21 14:37:19 +0100

    Update __init__.py

--- components/ithofan/__init__.py
+++ components/ithofan/__init__.py
@@ -1,1 +1,15 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.const import CONF_ID
 
+# This links the ithofan to the cc1101 radio component
+DEPENDENCIES = ['cc1101']
+ithofan_ns = cg.esphome_ns.namespace('ithofan')
+
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(ithofan_ns.IthoFan),
+}).extend(cv.COMPONENT_SCHEMA)
+
+def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    yield cg.register_component(var, config)
